f4a634fa89a343c48e24b23c96e0382d861771c3
xiaolong.liu <xiaolong.liu@energysh.com>
2020-07-24 15:59:49 +0800

    1

--- src/main/java/com/energysh/lib_common/ARouterPath.kt
+++ src/main/java/com/energysh/lib_common/ARouterPath.kt
@@ -7,9 +7,6 @@ class ARouterPath {
          * 相册选择界面
          * 相册选择界面
          * 相册选择界面
-         * 相册选择界面
-         *
-         *
          */
         const val AROUTER_GALLERY = "/quickart/gallery"
         /**
